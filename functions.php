f512fcbca43e2ae9e50d254442095ac21b1fde77
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 14:19:10 +0300

    fic enqeue css

--- functions.php
+++ functions.php
@@ -13,7 +13,6 @@ function theme_js() {
 	global $wp_scripts;
 
 	wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
-	wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '/js/scripts.js');
 
 }
 
