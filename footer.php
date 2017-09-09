b2883f8b4a5d164607992491dabe2bf36068a3c5
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 11:40:35 +0300

    fix header

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <footer class="bte-footer">
         <div class="row">
 
-        	<div class="col-sm-2 col-sm-offset-2">
+        	<div class="col-sm-2 col-sm-offset-4">
                 <img height="100px" src="<?php echo get_bloginfo( 'template_directory' );?>/gtu-white.png">
             </div>
     		<div class="col-sm-2">
