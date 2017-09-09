edd9a096da7f1ce2acd556ffee01024995fe434f
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 14:31:09 +0300

    fix menu

--- header.php
+++ header.php
@@ -26,31 +26,33 @@
 
 
  		<div class="bte-masthead container">
+			<br>
 
-
-		<nav class="navbar navbar-default">
-		  <div class="container-fluid">
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		      <a class="navbar-brand" href="#">Project name</a>
-		    </div>
-		    <div id="navbar" class="navbar-collapse collapse">
-		      <ul class="nav navbar-nav">
-		          <li><a href="<?php echo get_permalink( get_page_by_path('faculty'))?>">People</a></li>
-		          <li><a href="<?php echo get_permalink( get_page_by_path('research'))?>">Research</a></li>
-		          <li><a href="<?php echo get_permalink( get_page_by_path('projects'))?>">Projects</a></li>
-		      </ul>
-		      <ul class="nav navbar-nav navbar-right">
-		          <li><a href="<?php echo get_permalink( get_page_by_path('contact'))?>">Contact</a></li>
-		      </ul>
-		    </div><!--/.nav-collapse -->
-		  </div><!--/.container-fluid -->
-		</nav>
-	</div>
+			<nav class="navbar navbar-default">
+			  <div class="container-fluid">
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+				  <a class="navbar-brand" href="<?php echo get_permalink( get_page_by_path('home'))?>">
+				      <img height="50px" src="<?php echo get_bloginfo( 'template_directory' );?>/favicon.png">
+				  </a>
+			    </div>
+			    <div id="navbar" class="navbar-collapse collapse">
+			      <ul class="nav navbar-nav">
+			          <li><a href="<?php echo get_permalink( get_page_by_path('faculty'))?>">People</a></li>
+			          <li><a href="<?php echo get_permalink( get_page_by_path('research'))?>">Research</a></li>
+			          <li><a href="<?php echo get_permalink( get_page_by_path('projects'))?>">Projects</a></li>
+			      </ul>
+			      <ul class="nav navbar-nav navbar-right">
+			          <li><a href="<?php echo get_permalink( get_page_by_path('contact'))?>">Contact</a></li>
+			      </ul>
+			    </div><!--/.nav-collapse -->
+			  </div><!--/.container-fluid -->
+			</nav>
+		</div>
 
 	<div class="container">
