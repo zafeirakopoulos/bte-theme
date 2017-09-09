5e9d600c0f93238ea3b3b7bb217b3df662002681
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 12:33:31 +0300

    menu testing

--- header.php
+++ header.php
@@ -31,11 +31,33 @@
 
 				<div class="col-sm-7">
 					<h1 class="bte-title"><?php echo get_bloginfo( 'name' ); ?></h1>
-					<div class="container">
-						<nav class="bte-nav">
-							<a class="bte-nav-item active" href="#">Home</a><?php wp_list_pages( '&title_li=' ); ?>
-						</nav>
-					</div>
+					<!-- Static navbar -->
+			         <nav class="navbar navbar-default">
+			           <div class="container-fluid">
+			             <div class="navbar-header">
+			               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+			                 <span class="sr-only">Toggle navigation</span>
+			                 <span class="icon-bar"></span>
+			                 <span class="icon-bar"></span>
+			                 <span class="icon-bar"></span>
+			               </button>
+			               <a class="navbar-brand" href="#">Project name</a>
+			             </div>
+			             <div id="navbar" class="navbar-collapse collapse">
+			               <ul class="nav navbar-nav">
+			                 <li class="active"><a href="#">Home</a></li>
+			                 <li><a href="#">About</a></li>
+			                 <li><a href="#">Contact</a></li>
+			               </ul>
+			               <ul class="nav navbar-nav navbar-right">
+			                 <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
+			                 <li><a href="../navbar-static-top/">Static top</a></li>
+			                 <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+			               </ul>
+			             </div><!--/.nav-collapse -->
+			           </div><!--/.container-fluid -->
+			         </nav>
+
 				</div>
 		</div>
 
