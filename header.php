0e82e6d9320ed9eb3dd562709a83c2de142151df
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 13:36:35 +0300

    menu testing

--- header.php
+++ header.php
@@ -35,13 +35,11 @@
 			            <div class="navbar-header">
 			              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 			                <span class="sr-only">Toggle navigation</span>
-						 	<span class="icon-bar"></span>
+			                <span class="icon-bar"></span>
+			                <span class="icon-bar"></span>
 			                <span class="icon-bar"></span>
 			              </button>
-						  <a class="navbar-brand" href="<?php echo get_permalink( get_page_by_path('home'))?>">
-							  <img height="50px" src="<?php echo get_bloginfo( 'template_directory' );?>/favicon.png">
-						  </a>
-
+			              <a class="navbar-brand" href="#">Project name</a>
 			            </div>
 			            <div id="navbar" class="navbar-collapse collapse">
 			              <ul class="nav navbar-nav">
