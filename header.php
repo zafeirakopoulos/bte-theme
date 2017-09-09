32abedd7b18d26b74dec13242a83a3e8622489f5
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 13:45:57 +0300

    menu testing

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 
 	<title><?php echo get_bloginfo( 'name' ); ?></title>
 
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
 
 	<link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
@@ -30,30 +30,44 @@
  		<div class="bte-masthead container">
 					<h1 class="bte-title"><?php echo get_bloginfo( 'name' ); ?></h1>
 					<!-- Static navbar -->
-			        <nav class="navbar navbar-default">
-			          <div class="container-fluid">
-			            <div class="navbar-header">
-			              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false" aria-controls="navbar">
-			                <span class="sr-only">Toggle navigation</span>
-			                <span class="icon-bar"></span>
-			                <span class="icon-bar"></span>
-			                <span class="icon-bar"></span>
-			              </button>
-			              <a class="navbar-brand" href="#">Project name</a>
-			            </div>
-			            <div id="navbar" class="navbar-collapse collapse">
-			              <ul class="nav navbar-nav">
-							  <li><a href="<?php echo get_permalink( get_page_by_path('faculty'))?>">People</a></li>
-							  <li><a href="<?php echo get_permalink( get_page_by_path('research'))?>">Research</a></li>
-							  <li><a href="<?php echo get_permalink( get_page_by_path('projects'))?>">Projects</a></li>
-			              </ul>
-			              <ul class="nav navbar-nav navbar-right">
-							  <li><a href="<?php echo get_permalink( get_page_by_path('contact'))?>">Contact</a></li>
-			              </ul>
-			            </div><!--/.nav-collapse -->
-			          </div><!--/.container-fluid -->
-			        </nav>
-
+					<!-- Static navbar -->
+		        <nav class="navbar navbar-default">
+		          <div class="container-fluid">
+		            <div class="navbar-header">
+		              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		                <span class="sr-only">Toggle navigation</span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		              </button>
+		              <a class="navbar-brand" href="#">Project name</a>
+		            </div>
+		            <div id="navbar" class="navbar-collapse collapse">
+		              <ul class="nav navbar-nav">
+		                <li class="active"><a href="#">Home</a></li>
+		                <li><a href="#">About</a></li>
+		                <li><a href="#">Contact</a></li>
+		                <li class="dropdown">
+		                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		                  <ul class="dropdown-menu">
+		                    <li><a href="#">Action</a></li>
+		                    <li><a href="#">Another action</a></li>
+		                    <li><a href="#">Something else here</a></li>
+		                    <li role="separator" class="divider"></li>
+		                    <li class="dropdown-header">Nav header</li>
+		                    <li><a href="#">Separated link</a></li>
+		                    <li><a href="#">One more separated link</a></li>
+		                  </ul>
+		                </li>
+		              </ul>
+		              <ul class="nav navbar-nav navbar-right">
+		                <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
+		                <li><a href="../navbar-static-top/">Static top</a></li>
+		                <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+		              </ul>
+		            </div><!--/.nav-collapse -->
+		          </div><!--/.container-fluid -->
+		        </nav>
 		</div>
 
 	<div class="container">
