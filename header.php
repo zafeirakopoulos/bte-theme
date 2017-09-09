fe0548355eccd3af2d4dff75662b05e520c16c06
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 11:48:04 +0300

    favicon

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
 	<title><?php echo get_bloginfo( 'name' ); ?></title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
