447e7ce7c3d813aa104efa29564a10d316bc726f
Zafeirakis Zafeirakopoulos <zafeirakopoulos@gmail.com>
2017-09-09 13:24:32 +0300

    menu testing

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 </footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" rel="stylesheet">
 <?php wp_footer(); ?>
 </body>
 </html>
